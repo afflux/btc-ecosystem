d314e7503b6e74d372a548f8ea060487fb30a464
Martin Keßler <martin@moegger.de>
2014-05-13 14:33:14 +0200

    vivado start script

---
+++ vivado/vivado.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LC_NUMERIC="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+source /opt/Xilinx/Vivado/2014.1/settings64.sh
+
+tmpdir=$(mktemp -d)
+cd $tmpdir
+
+vivado
+
+rm -rf $tmpdir
